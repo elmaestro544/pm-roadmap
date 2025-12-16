0566066c58aa62a32f96e0b5cd8c86b242a7a68d
elmaestro544 <oxygen.ml1@gmail.com>
2025-12-16 15:01:46 +0300

    Add files via upload

--- env.js
+++ env.js
@@ -1,3 +1,4 @@
+
 // This file is configured for runtime environment variable injection.
 // In Coolify/Docker, use a Post-Deployment command to replace the placeholders.
 // Example: sed -i "s|__VITE_API_KEY__|$VITE_API_KEY|g" env.js
@@ -8,6 +9,7 @@ window.process.env = window.process.env || {};
 // --- API Keys & Configuration ---
 
 // Maps standard keys to the placeholders
+// The user's sed command replaces __VITE_API_KEY__
 window.process.env.API_KEY = '__VITE_API_KEY__';
 window.process.env.SUPABASE_URL = '__VITE_SUPABASE_URL__';
 window.process.env.SUPABASE_ANON_KEY = '__VITE_SUPABASE_ANON_KEY__';
